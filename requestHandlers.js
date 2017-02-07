621b9ef05122a702a594e42f788cda72c448257e
jfIvan <tangxiaojun_123@126.com>
2017-02-07 11:58:13 +0800

    add folder app and dist represent back-end and fore-end respectively. add file upload function use POST method. ......

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,12 @@
 
-var exec = require("child_process").exec
+// var exec = require("child_process").exec
 var querystring = require("querystring")
+var fs = require("fs")
+var formidable = require("formidable")
 
-function start(response, postData) {
+var imgUploadPath = "/root/tang_project/liberate/dist/images/jfx01.jpg"
+
+function start(response) {
 	console.log("Requet handler 'start' was called.")
 
 	// exec("find /", 
@@ -19,9 +23,10 @@ function start(response, postData) {
 	    'charset=UTF-8" />'+
 	    '</head>'+
 	    '<body>'+
-	    '<form action="/upload" method="post">'+
-	    '<textarea name="text" rows="20" cols="60"></textarea>'+
-	    '<input type="submit" value="Submit text" />'+
+	    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+	    // '<textarea name="text" rows="20" cols="60"></textarea>'+
+	    '<input type="file" name="upload" multiple="multiple">'+
+	    '<input type="submit" value="Upload file" />'+
 	    '</form>'+
 	    '</body>'+
 	    '</html>';
@@ -31,12 +36,38 @@ function start(response, postData) {
 	response.end()
 }
 
-function upload(response, postData) {
+function upload(response, /*postData*/request) {
 	console.log("Requet handler 'upload' was called.")
-	response.writeHead(200, {"Content-Type": "text/plain"})
-	response.write("You've sent the text: " + querystring.parse(postData).text)
-	response.end()
+
+	var form = new formidable.IncomingForm()
+	console.log("about to parse")
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done")
+		fs.renameSync(files.upload.path, imgUploadPath)
+		response.writeHead(200, {"Content-Type": "text/html"})
+		// response.write("You've sent the text: " + querystring.parse(postData).text)
+		response.write("received image: <br/>")
+		response.write("<img src='/show' />")
+		response.end()
+	})
+}
+
+function show(response, postData) {
+	console.log("Requet handler 'show' was called.")
+	fs.readFile(imgUploadPath, "binary", 
+		function(error, file) {
+		if(error) {
+			response.writeHead(500, {"Content-Type": "text/plain"})
+			response.write(error + "\n")
+			response.end()
+		} else {
+			response.writeHead(200, {"Content-Type": "text/png"})
+			response.write(file, "binary")
+			response.end()
+		}
+	})
 }
 
 exports.start = start
 exports.upload = upload
+exports.show = show
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var http = require("http")
 var url = require("url")
+var formidable = require("formidable")
 
 function start(route, handle){
 	
@@ -9,16 +10,18 @@ function start(route, handle){
 		var pathname = url.parse(request.url).pathname
 		console.log("Request for " + pathname + " received.")
 
-		request.setEncoding("utf8")
+		// request.setEncoding("utf8")
 
-		request.addListener("data", function(postDataChunk) {
-			postData += postDataChunk
-			console.log("Received POST data chunk '" + postDataChunk + "'.")
-		})
+		// request.addListener("data", function(postDataChunk) {
+		// 	postData += postDataChunk
+		// 	console.log("Received POST data chunk '" + postDataChunk + "'.")
+		// })
 
-		request.addListener("end", function() {
-			route(handle, pathname, response, postData)
-		})
+		// request.addListener("end", function() {
+		// 	route(handle, pathname, response, postData)
+		// })
+
+		route(handle, pathname, response, request)
 	}
 
 	http.createServer(onRequest).listen(8888)
